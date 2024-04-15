3ca21e38909473c53ac907625cbad6dae89b520d
RayRaf <rayraf@mail.ru>
2024-04-15 14:53:06 +0500

    Добавлена клавиатура

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import telebot
+from telebot import types
 from secrets_1 import TOKEN
-
 # Настройка базы данных
 Base = declarative_base()
 
@@ -44,6 +44,13 @@ def get_commands_list():
     /help - Показать этот список команд
     """
 
+def create_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    commands = ['/new_task Новая задача', '/tasks Список задач', '/rmtasks Удалить все', '/rmtask Удалить задачу', '/help Помощь']
+    buttons = [types.KeyboardButton(command) for command in commands]
+    keyboard.add(*buttons)
+    return keyboard
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
@@ -52,13 +59,13 @@ def send_welcome(message):
         new_user = User(chat_id=chat_id, first_name=message.chat.first_name, username=message.chat.username)
         session.add(new_user)
         session.commit()
-        bot.reply_to(message, "Теперь ты зарегистрирован! Используй команды для управления задачами." + get_commands_list())
+        bot.reply_to(message, "Теперь ты зарегистрирован! Используй команды для управления задачами.", reply_markup=create_keyboard())
     else:
-        bot.reply_to(message, "Ты уже зарегистрирован." + get_commands_list())
+        bot.reply_to(message, "Ты уже зарегистрирован." + get_commands_list(), reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.reply_to(message, get_commands_list())
+    bot.reply_to(message, get_commands_list(), reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['new_task'])
 def new_task(message):
@@ -66,9 +73,9 @@ def new_task(message):
     user = session.query(User).filter_by(chat_id=chat_id).first()
     if user:
         expecting_task[chat_id] = True
-        bot.reply_to(message, "Отправь мне текст задачи.")
+        bot.reply_to(message, "Отправь мне текст задачи.", reply_markup=create_keyboard())
     else:
-        bot.reply_to(message, "Вы не зарегистрированы в системе. Используйте /start для регистрации.")
+        bot.reply_to(message, "Вы не зарегистрированы в системе. Используйте /start для регистрации.", reply_markup=create_keyboard())
 
 @bot.message_handler(func=lambda message: message.chat.id in expecting_task and expecting_task[message.chat.id])
 def add_task(message):
@@ -86,9 +93,9 @@ def add_task(message):
 
         user.task_index += 1  # Увеличиваем индекс следующей задачи
         del expecting_task[chat_id]
-        bot.reply_to(message, f"Задача добавлена: {message.text}")
+        bot.reply_to(message, f"Задача добавлена: {message.text}", reply_markup=create_keyboard())
     else:
-        bot.reply_to(message, "Произошла ошибка, возможно, вы не зарегистрированы.")
+        bot.reply_to(message, "Произошла ошибка, возможно, вы не зарегистрированы.", reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['tasks'])
 def show_tasks(message):
@@ -100,38 +107,38 @@ def show_tasks(message):
             reply = "\n".join([f"{task.local_id}. {task.text}" for task in tasks])
         else:
             reply = "Список задач пуст."
-        bot.reply_to(message, reply)
+        bot.reply_to(message, reply, reply_markup=create_keyboard())
     else:
-        bot.reply_to(message, "Вы не зарегистрированы в системе.")
+        bot.reply_to(message, "Вы не зарегистрированы в системе.", reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['rmtasks'])
 def remove_tasks(message):
     chat_id = message.chat.id
     user = session.query(User).filter_by(chat_id=chat_id).first()
     if user:
-        bot.send_message(chat_id, "Вы уверены, что хотите удалить все ваши задачи? (ответьте '1' для подтверждения)")
+        bot.send_message(chat_id, "Вы уверены, что хотите удалить все ваши задачи? (ответьте '1' для подтверждения)", reply_markup=create_keyboard())
         bot.register_next_step_handler(message, process_task_removal_confirmation, user)
     else:
-        bot.reply_to(message, "Вы не зарегистрированы в системе.")
+        bot.reply_to(message, "Вы не зарегистрированы в системе.", reply_markup=create_keyboard())
 
 def process_task_removal_confirmation(message, user):
     if message.text == '1':
         user.tasks = []  # Удаляем все задачи пользователя
         user.task_index = 1  # Сброс индекса задач
         session.commit()
-        bot.reply_to(message, "Все ваши задачи удалены.")
+        bot.reply_to(message, "Все ваши задачи удалены.", reply_markup=create_keyboard())
     else:
-        bot.reply_to(message, "Операция отменена.")
+        bot.reply_to(message, "Операция отменена.", reply_markup=create_keyboard())
 
 @bot.message_handler(commands=['rmtask'])
 def remove_task(message):
     chat_id = message.chat.id
     user = session.query(User).filter_by(chat_id=chat_id).first()
     if user:
-        bot.send_message(chat_id, "Введите номер задачи для удаления:")
+        bot.send_message(chat_id, "Введите номер задачи для удаления:", reply_markup=create_keyboard())
         bot.register_next_step_handler(message, process_task_removal_input, user)
     else:
-        bot.reply_to(message, "Вы не зарегистрированы в системе.")
+        bot.reply_to(message, "Вы не зарегистрированы в системе.", reply_markup=create_keyboard())
 
 def process_task_removal_input(message, user):
     try:
@@ -144,14 +151,14 @@ def process_task_removal_input(message, user):
             for index, task in enumerate(sorted(user.tasks, key=lambda x: x.local_id), start=1):
                 task.local_id = index
             session.commit()
-            bot.reply_to(message, f"Задача {task_id} удалена.")
+            bot.reply_to(message, f"Задача {task_id} удалена.", reply_markup=create_keyboard())
         else:
-            bot.reply_to(message, f"Задача с номером {task_id} не найдена.")
+            bot.reply_to(message, f"Задача с номером {task_id} не найдена.", reply_markup=create_keyboard())
     except ValueError:
-        bot.reply_to(message, "Неправильный формат номера задачи. Пожалуйста, введите число.")
+        bot.reply_to(message, "Неправильный формат номера задачи. Пожалуйста, введите число.", reply_markup=create_keyboard())
 
 @bot.message_handler(func=lambda message: True)
 def handle_unregistered_command(message):
-    bot.reply_to(message, "Неправильная команда. " + get_commands_list())
+    bot.reply_to(message, "Неправильная команда. " + get_commands_list(), reply_markup=create_keyboard())
 
 bot.polling()
